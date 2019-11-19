650e6afa75a7212553b9fdaf84a42e705d3d5ce9
Jeremías ramírez <30301246+jeremiasramirez@users.noreply.github.com>
2019-11-19 15:42:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,94 +13,95 @@
         if(!document.getElementById("containerMenu")){
 
 
-        /*
-         ***********   functions   **************
-        */
+            /*
+             ***********   functions   **************
+            */
 
-        function createItem(itemMenu, containerMenu){
+            function createItem(itemMenu, containerMenu){
 
-            /*specify 6 item to create*/
-            for(let i=0; i<6; i++){
+                /*specify 6 item to create*/
+                for(let i=0; i<6; i++){
 
-                //creating item of menu
-                itemMenu = document.createElement("li");
-                itemMenu.setAttribute("id", "item-" + i);
-                itemMenu.setAttribute("class", "item-" + i);
-                itemMenu.textContent = textItemMenu[i];
+                    //creating item of menu
+                    itemMenu = document.createElement("li");
+                    itemMenu.setAttribute("id", "item-" + i);
+                    itemMenu.setAttribute("class", "item-" + i);
+                    itemMenu.textContent = textItemMenu[i];
 
-                /*adding item to menu*/
-                containerMenu.appendChild(itemMenu);
+                    /*adding item to menu*/
+                    containerMenu.appendChild(itemMenu);
 
-            }
-
-        }
-
-
-
-
-        function placeMenu(menu, order){
-
-            menu.style.top = ( e.clientY - order )  + "px";
-            menu.style.left = ( e.clientX + order ) + "px";
-        }
-
-
-
-
-        function removeMenu(menu){
-
-            if(menu){
-
-                menu.addEventListener("click", ()=>{
-                    menu.remove();
-                }, false);
+                }
 
             }
 
-        }
+
+
+
+            function placeMenu(menu, order){
+
+                menu.style.top = ( e.clientY - order )  + "px";
+                menu.style.left = ( e.clientX + order ) + "px";
+            }
 
 
 
 
-        function containerMenuFunc(containerMenu){
+            function removeMenu(menu){
 
-            containerMenu.textContent = "Menu  " +  "  X";
-            containerMenu.setAttribute("id", "containerMenu");
-            containerMenu.setAttribute("class", "containerMenu");
+                if(menu){
 
-        }
+                    menu.addEventListener("click", ()=>{
+                        menu.remove();
+                    }, false);
 
+                }
 
+            }
 
-
-        /*********** End functions  ***********/
-
-
-        let containerMenu = document.createElement("ul");
-
-        containerMenuFunc(containerMenu);
-
-        let itemMenu = null,
-            textItemMenu = ["Open", "Edit", "Delete", "Copy", "Paste", "Close"];
-
-        createItem(itemMenu, containerMenu);
+  
+            
 
 
-        /*adding menu to body*/
-        document.body.appendChild(containerMenu);
+            function containerMenuFunc(containerMenu){
+
+                containerMenu.textContent = "Menu  " +  "  X";
+                containerMenu.setAttribute("id", "containerMenu");
+                containerMenu.setAttribute("class", "containerMenu");
+
+            }
 
 
-        /*  get menu id*/
-        let menu = document.getElementById("containerMenu"),
-            order = 15;
 
 
-        /*place of menu*/
-        placeMenu(menu, order);
+            /*********** End functions  ***********/
 
 
-        /*if menu is clicked then removed*/
-        removeMenu(menu);
+            let containerMenu = document.createElement("ul");
+
+            containerMenuFunc(containerMenu);
+
+            let itemMenu = null,
+                textItemMenu = ["Open", "Edit", "Delete", "Copy", "Paste", "Close"];
+
+            createItem(itemMenu, containerMenu);
+
+
+            /*adding menu to body*/
+            document.body.appendChild(containerMenu);
+
+
+            /*  get menu id*/
+            let menu = document.getElementById("containerMenu"),
+                order = 15;
+
+
+            /*place of menu*/
+            placeMenu(menu, order);
+
+
+            /*if menu is clicked then removed*/
+            removeMenu(menu);
 
 
         }
